00ac2b9667b58f4d3e3695e3b5142ddd098f0525
Stephon Smith <smithwrestling89@gmail.com>
2020-06-23 18:44:01 -0400

    added functionality to submit and dropdown selectors to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    // Stops button from submitting data and refreshing page
     event.preventDefault();
+    // Storing the values of the input and dropdown box in variables
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // Adding an li to the UL with class task-item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // Creating a div below the li with class task-info
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // Inserting HTML to display stored values of Input and Dropdown
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" +
+    taskTypeInput + "</span>";
+    // Adding them as child elements of the UL
     tasksToDoEl.appendChild(listItemEl);
-  }
+    listItemEl.appendChild(taskInfoEl);
+ }
 
-  buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
