13a8c37abea0e3ea61fb1255e0f87d94f6b2ee18
Stephon Smith <smithwrestling89@gmail.com>
2020-06-23 19:16:34 -0400

    add abilit to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,42 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
     // Stops button from submitting data and refreshing page
     event.preventDefault();
     // Storing the values of the input and dropdown box in variables
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    // Adding an li to the UL with class task-item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    // Creating a div below the li with class task-info
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    // Inserting HTML to display stored values of Input and Dropdown
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" +
-    taskTypeInput + "</span>";
-    // Adding them as child elements of the UL
-    tasksToDoEl.appendChild(listItemEl);
-    listItemEl.appendChild(taskInfoEl);
- }
 
-formEl.addEventListener("submit", createTaskHandler);
+    if (!taskNameInput || !taskTypeInput) {
+      alert("You need to fill out the task form!");
+      return false;
+    }
+    
+    formEl.reset();
+    
+    // Package data as an object
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+ }
+var createTaskEl = function(taskDataObj) {
+  // Adding an li to the UL with class task-item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // Creating a div below the li with class task-info
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  // Inserting HTML to display stored values of Input and Dropdown
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" +
+    taskDataObj.type + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+  tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
