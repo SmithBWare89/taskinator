2928a988f34e45c49cdfee43af0c3e8ee47c844e
Stephon Smith <smithwrestling89@gmail.com>
2020-06-23 12:05:57 -0400

    added form-submit data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@ var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task";
-    tasksToDoEl.appendChild(taskItemEl);
-};
+    event.preventDefault();
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
 
-buttonEl.addEventListener("click", createTaskHandler)
+  buttonEl.addEventListener("click", createTaskHandler);
